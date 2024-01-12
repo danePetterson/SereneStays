1f0a98e7059cc209a0e175404337bf7641e8162d
danePetterson <danepetterson@gmail.com>
2024-01-11 21:10:38 -0800

    Get all Spots endpoint is complete, hooray!

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -23,6 +23,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'SpotImage',
+    timestamps: false
   });
   return SpotImage;
 };
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,11 @@
 // Imports ----------------------------
-
+const sequelize = require('sequelize')
 const express = require('express');
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { Spot } = require('../../db/models');
+const { Spot, SpotImage, Review } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -15,8 +15,37 @@ const router = express.Router();
 
 //---------------------------------------
 
+//Get all Spots
+
 router.get('/', async (req, res, next) => {
-    const allSpots = await Spot.findAll();
+    const allSpots = await Spot.findAll({
+        include: [
+            {
+                model: Review,
+                attributes: [
+                    [
+                        sequelize.fn('AVG', sequelize.col('stars')), 'avgRating'
+                    ]
+                ]
+            },
+            {
+                model: SpotImage,
+                attributes: ['url'],
+                where: {preview: true},
+                limit: 1
+            }
+        ],
+        group: ['Spot.id']
+    });
+
+    allSpots.forEach(element => {
+       element.dataValues.avgRating = element.dataValues.Reviews[0].dataValues.avgRating;
+       element.dataValues.previewImage = element.dataValues.SpotImages[0].dataValues.url;
+
+       delete element.dataValues.Reviews;
+       delete element.dataValues.SpotImages;
+    });
+
     res.json(allSpots)
 })
 
