eac19e2b337a732a00b76aa1a50372c2d9e266c9
danePetterson <danepetterson@gmail.com>
2024-02-05 14:11:49 -0800

    fixed even more group bys

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -385,7 +385,7 @@ router.get('/:spotId', async (req, res, next) => {
                 attributes: ['id','firstName','lastName']
             }
         ],
-        group: ['Spot.id', 'Reviews.id']
+        group: ['Spot.id', 'Reviews.id', 'SpotImages.id']
     })
 
     //not found error handle
